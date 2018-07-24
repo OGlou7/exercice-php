8cf206759470abdae3f79a52df7eff13238794a5
[MLouise] <[mlouiseogdoc@gmail.com]>
2018-07-24 16:16:58 +0200

    css excuse

--- excuse.php
+++ excuse.php
@@ -1,14 +1,15 @@
 
 <?php
- if(isset($_GET["button"])){
-   $nom = $_GET["nom"];
-   $prenom = $_GET["prenom"];
-   $prof = $_GET["prof"];
-   $excuse = $_GET["excuse"];
-   $genre = $_GET['genre'];
+ if(isset($_POST["button"])){
+   @$nom = $_POST["nom"];
+   @$prenom = $_POST["prenom"];
+   @$prof = $_POST["prof"];
+   @$excuse = $_POST["excuse"];
+   @$genre = $_POST['genre'];
  }
+                // @ BEFORE VAR is to hide the Notice in HTML
 //GENRE
-  if ($genre == 'Fille') {
+ if (@$genre == 'Fille') {
     $genre = 'elle';
   }
   else {
@@ -16,21 +17,26 @@
   }
 
 //EXCUSES
-  if ($excuse == "malade") {
-    $text = " est tombé malade cette nuit. Le docteur prévoit une reprise des cours pour la fin de semaine.";
+
+  if (@$excuse == "malade") {
+    $text = " est tombé malade cette nuit. <br /> Le docteur prévoit une reprise des cours pour la fin de semaine.";
   }
-  elseif ($excuse == "familiales") {
+  elseif (@$excuse == "familiale") {
     $text = " a perdu un membre de sa famille.";
   }
-  elseif ($excuse == "voyage") {
+  elseif (@$excuse == "voyage") {
     $text =" est à l'étranger et reviendra ce lundi. ";
   }
-  elseif ($excuse == "intoxication") {
-    $text =" a eu un intoxication alimentaire. Le docteur prévoit une reprise des cours la semaine prochaine.";
+  elseif (@$excuse == "intoxication") {
+    $text =" a eu un intoxication alimentaire. <br /> Le docteur prévoit une reprise des cours la semaine prochaine.";
   }
-  elseif ($excuse == "extra-scolaire") {
+  elseif (@$excuse == "extra-scolaire") {
     $text =" a un match SUPER important. ";
   }
+
+//SANITIZE ALL
+  $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
 ?>
 
 
@@ -38,50 +44,54 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title> Générateur D'Excuses </title>
+    <link rel="stylesheet" href="./style.css">
+    <title> Générateur d'Excuses </title>
   </head>
   <body>
-    <form class="" action="" method="get">
-      <h1>Formulaire d'absence</h1>
-      Nom <br>
-      <input type="text" name="nom" size="35" value=""><br>
-      Prénom <br>
-      <input type="text" name="prenom" size="35" value=""><br>
+    <form class="" action="" method="post">
+      <h1>Générateur d'Excuses</h1>
+      <strong>Nom</strong><br>
+      <input type="text" name="nom" size="35" value="" placeholder="Nom de l'enfant..."><br><br>
+      <strong>Prénom</strong><br>
+      <input type="text" name="prenom" size="35" value="" placeholder="Prénom de l'enfant..."><br>
       <br>
-      <input type="radio" name="genre" value="Fille">Fille
+      <input type="radio" name="genre" value="Fille">Fille &nbsp
       <input type="radio" name="genre" value="Garçon">Garçon <br>
       <br>
-      Nom du professeur <br>
-      <input type="text" name="prof" size="35" value=""><br>
+      <strong>Nom du professeur</strong><br>
+      <input type="text" name="prof" size="35" value="" placeholder="Nom du professeur..."><br>
       <br>
-      Raison de l'absence
+      <strong>Raison de l'absence </strong><br>
       <label> <br>
-        <input name="excuse" type="radio" value="malade">malade
+        <input name="excuse" type="radio" value="malade">malade &nbsp
       </label>
       <label>
-        <input name="excuse" type="radio" value="familiales">familiales.
+        <input name="excuse" type="radio" value="familiale">familiale &nbsp
       </label>
       <label>
-        <input name="excuse" type="radio" value="voyage">voyage
+        <input name="excuse" type="radio" value="voyage">voyage &nbsp
       </label>
       <label>
-        <input name="excuse" type="radio" value="intoxication">intoxication
+        <input name="excuse" type="radio" value="intoxication">intoxication &nbsp
       </label>
       <label>
-        <input name="excuse" type="radio" value="extra-scolaire">activité extra-scolaire
-      </label><br>
-      <button type="submit" name="button"> Envoyer</button>
+        <input name="excuse" type="radio" value="extra-scolaire">activité extra-scolaire &nbsp
+      </label><br><br>
+      <button type="submit" name="button"> Envoyer</button> <br><br>
 
+      <section>
       <h3>
-      <p><?php echo $prof ?>,<br>
-      Je vous prie d'excuser l'absence ce <?php echo date('d/m/y')?>. <br>
-      En effet,<?= $_GET['nom'] ?> ne pourra pas être présent à l'école aujourd'hui car
-      <?= $genre ?> <?= $text ?><br>
+      <p><?php echo @$prof ?>,<br>
+      Je vous prie d'excuser l'absence ce <?php echo date('l d/m/y')?>. <br>
+      En effet, notre enfant <?= @$_POST['prenom'] ?> <?= @$_POST['nom'] ?> ne pourra pas être présent à l'école aujourd'hui car
+      <?= $genre ?> <?= @$text ?><br>
       Pour tout complément d’informations n’hésitez surtout pas à nous contacter.
       <br><br>
-      Cordialement <br>
-      Signature parentale.</p>
-    </h3>
+      Cordialement,<br><br>
+      Signature parentale.</p><br>
+      </h3>
+      </section>
     </form>
+
   </body>
 </html>
